ecbb8122fabe07c2e7d388ff4b5f1ea051ba1207
ic21b044@technikum-wien.at <nbwhiDZJaYRxVGTu6Lgs>
2022-12-03 21:01:02 +0100

    Initial commit

---
+++ src/main/java/fh/damo/slmvor/SlmvorApplication.java
@@ -0,0 +1,13 @@
+package fh.damo.slmvor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmvorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmvorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fh/damo/slmvor/SlmvorApplicationTests.java
@@ -0,0 +1,13 @@
+package fh.damo.slmvor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmvorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
